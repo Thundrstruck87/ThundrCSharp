f569a35203b87b60bb20c67e7f9d830c18c0d5dc
TJ Struck <74696796+Thundrstruck87@users.noreply.github.com>
2021-03-20 10:10:57 -0500

    Vinyl Record GUI
    This was the hardest program I've worked on so far in my progress of learning C#. I had a lot of fun doing it. It is not completely done, I want to go over the code as I learn more efficient ways of C# to improve my code. Eventually I would love to have this available to where it does not need a SQL database. All in due time! For now, this uses an existing SQL Database, and configure Windows Forms in Visual Studio.

---
+++ Vinyl Record Catalog.cs
@@ -0,0 +1,84 @@
+// This program utilizes an already existing SQL Database and Windows Form //
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient; // This needs to be added for SQL Integration
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vinyl_Catalog
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // ----- SQL Database Connection ----- //
+
+            string connectionString;
+            SqlConnection cnn;
+            connectionString = @"Data Source=[server];Initial Catalog=catalog;User ID=sa;Password=[password]";
+            cnn = new SqlConnection(connectionString);
+            cnn.Open();
+
+            // ----- Commands to show tables ----- //
+
+            SqlCommand command;
+            SqlDataReader dataReader;
+            String sql, Output = "";
+
+            sql = "Select artist,album,year from vinyl";
+
+            command = new SqlCommand(sql, cnn);
+            dataReader = command.ExecuteReader();
+
+            while (dataReader.Read())
+            {
+                Output = Output + dataReader.GetValue(0) + " - " + dataReader.GetValue(1) + " - " + dataReader.GetValue(2) + "\n"; // ----- this section displays the output ----- //
+                // ----- dataReader.GetValue(0) is the first column, dataReader.GetValue(1) is the second column, dataReader.GetValue(2) is the third column. First position index to 0 ----- //
+            }
+            MessageBox.Show(Output);
+            
+            dataReader.Close();
+            command.Dispose();
+            cnn.Close();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'catalogDataSet.vinyl' table. You can move, or remove it, as needed.
+            this.vinylTableAdapter.Fill(this.catalogDataSet.vinyl);
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=[server];Initial Catalog=catalog;User ID=sa;Password=[password]");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("insert into vinyl(artist, album, year) values ('"+txt_artist.Text+"','"+txt_album.Text+"','"+txt_year.Text+"')", con);
+            int i = cmd.ExecuteNonQuery();
+            if (i != 0)
+            {
+                MessageBox.Show("The album has been added!");
+                this.catalogDataSet.Reset();
+                this.vinylTableAdapter.Fill(this.catalogDataSet.vinyl);
+            }
+            else
+            {
+                MessageBox.Show("error");
+            }
+            con.Close();            
+        }
+    }
+}
